25dda492200bf8518d353a917d8b181423462a2d
Guillermo <guillermo.gaete@tiendanube.com>
2024-06-07 16:06:36 -0300

    view pr diff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from github import Auth
 from github import Github
-
+import os 
 class GithubData:
     def __init__(self, repo, access_token) -> None:
         auth = Auth.Token(access_token)
@@ -23,3 +23,7 @@ class GithubData:
 
         return list(map(lambda commit: commit.commit.message, commits))
 
+if __name__ == "__main__":
+    githubData = GithubData(os.getenv('REPO_PATH'), os.getenv('GITHUB_ACTIONS_TOKEN'))
+    pr_diff = githubData.get_pr_diff(1)
+    print(pr_diff)
